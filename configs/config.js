e1508ce9a94e7d519da3967453cef2c4cc85f781
Shreyash Gedam <shreyashakgedam@gmail.com>
2023-06-16 11:02:31 +0530

    second commit

--- configs/config.js
+++ configs/config.js
@@ -1,5 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
-const connection = mongoose.connect("mongodb://localhost:27017/voosh");
+const connection = mongoose.connect(
+  "mongodb+srv://Shreyash:shreyash1234@cluster0.eyxgk.mongodb.net/voosh?retryWrites=true&w=majority"
+);
 
 module.exports = connection;
+ 
